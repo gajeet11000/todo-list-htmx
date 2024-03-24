e98e5f429c3f84a6a3fcce6e07c96359128add46
G Ajeet <gajeet11000@gmail.com>
2024-03-24 15:59:38 +0530

    Added List and Task models with migrations

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class List(models.Model):
+    title = models.CharField(max_length=24, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
+    date = models.DateTimeField(auto_now_add=True)
+    
+class Task(models.Model):
+    task = models.CharField(max_length=64)
+    list_id = models.ForeignKey(List, on_delete=models.CASCADE, null=True)
+    
+    date = models.DateTimeField(auto_now_add=True)
