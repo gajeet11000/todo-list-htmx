34e843bcf450ea6f2b1092c9d573a5777b14e579
G Ajeet <gajeet11000@gmail.com>
2024-03-24 18:05:11 +0530

    Added register and login views

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,8 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from datetime import date
 
-# Create your views here.
+from django.contrib import auth
+
+from . models import List, Task
+from . forms import CreateUserForm, LoginForm
+
+
+
 def index(req):
     return render(req, "todo_app/index.html")
+
+def register(req):
+    if req.method == "GET":
+        
+        new_form = CreateUserForm()
+        return render(req, "todo_app/register.html", {"form": new_form})
+    
+    elif req.method == "POST":
+        
+        filled_form = CreateUserForm(req.POST)
+        if filled_form.is_valid():
+            filled_form.save()
+            return redirect("login")
+
+def login(req):
+    if req.method == "GET":
+        
+        new_form = LoginForm()
+        return render(req, "todo_app/register.html", {"form": new_form})
+    
+    elif req.method == "POST":
+        
+        filled_form = LoginForm(req, req.POST)
+        
+        if filled_form.is_valid():
+            username = req.POST.get("username")
+            password = req.POST.get("password")
+            
+            user = auth.authenticate(req, username=username, password=password)
+            
+            if user is not None:
+                auth.login(req, user)
+                return redirect("dashboard")
+    
 
 def dashboard(req):
     return render(req, "todo_app/dashboard.html")
