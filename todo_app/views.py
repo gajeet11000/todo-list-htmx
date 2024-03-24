d8eb6b8dbaad350dcb574b14cf60c209e0ff5423
G Ajeet <gajeet11000@gmail.com>
2024-03-24 13:37:45 +0530

    Added dashboard html and its required url, view, and navbar link

--- todo_app/views.py
+++ todo_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(req):
     return render(req, "todo_app/index.html")
+
+def dashboard(req):
+    return render(req, "todo_app/dashboard.html")
