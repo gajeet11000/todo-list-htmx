817003c5318305263f0ef3510cfaf4aabbee6c1a
G Ajeet <gajeet11000@gmail.com>
2024-03-26 03:57:09 +0530

    Added django-htmx to installed apps and its middleware

--- todo_list_htmx_project/settings.py
+++ todo_list_htmx_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo_app',
+    'django_htmx',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = 'todo_list_htmx_project.urls'
