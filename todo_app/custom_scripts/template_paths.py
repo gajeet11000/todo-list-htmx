75d2b0e9a1926fab353a577a385914a640b04b51
G Ajeet <gajeet11000@gmail.com>
2024-04-11 19:26:59 +0530

    separating partials

---
+++ todo_app/custom_scripts/template_paths.py
@@ -0,0 +1,26 @@
+templates = {
+    "index":"todo_app/index.html",
+    "login":"todo_app/login.html",
+    "logout":"todo_app/logout.html",
+    "register":"todo_app/register.html",
+    "dashboard":"todo_app/dashboard.html",
+}
+
+dashboard_contents = {
+    "filter": "todo_app/dashboard_contents/filter_content.html",
+    
+    "list_title": "todo_app/dashboard_contents/list_title_content.html",
+    
+    "search_bar": "todo_app/dashboard_contents/search_bar_content.html",
+    
+    "search_result": "todo_app/dashboard_contents/search_result_content.html",
+    
+    "single_task": "todo_app/dashboard_contents/single_task_content.html",
+    
+    "todo_list": "todo_app/dashboard_contents/todo_list_content.html",
+}
+
+patch_contents = {
+    "list": "todo_app/patch_contents/list_content.html",
+    "task": "todo_app/patch_contents/task_content.html"
+}
